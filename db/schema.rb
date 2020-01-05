bd8dc44a60ea09d35015135899b5e54e4f9ff282
Pancho Labrador <plabradorjr@gmail.com>
2020-01-05 09:11:42 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -12,4 +12,12 @@
 
 ActiveRecord::Schema.define(version: 2020_01_05_090104) do
 
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.integer "release_date"
+    t.string "director"
+    t.string "lead"
+    t.boolean "in_theaters"
+  end
+
 end
